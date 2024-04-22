e32263666273964202dc356d617380c9706a81df
Aleksey <vmstr@vmstr.ru>
2024-04-22 02:00:43 +0200

    added checkbox logic and states

--- src/stores/FilterStore.jsx
+++ src/stores/FilterStore.jsx
@@ -1,10 +1,28 @@
 import { makeAutoObservable } from "mobx";
 
 class FilterStore {
-  setFilters = [];
+  usedFilters = [];
 
   constructor() {
-    
+    makeAutoObservable(this);
+  }
+
+  addFilters(stops) {
+    const newStops = stops instanceof Array ? stops : [stops];
+    const uniqueStops = new Set(this.usedFilters.concat(newStops));
+    this.usedFilters = Array.from(uniqueStops);
+  }
+
+  setOnly(stops) {
+    this.usedFilters = [stops];
+  }
+  delFilters(stops) {
+    const newStopsSet = new Set(stops instanceof Array ? stops : [stops]);
+    this.usedFilters = this.usedFilters.filter(
+      (item) => !newStopsSet.has(item)
+    );
   }
 }
+
+
 export default new FilterStore();
